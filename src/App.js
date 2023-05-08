c2d64a7e168a3729102767d8bb93e72da089819f
aldrin <aldrins6@gmail.com>
2023-05-08 15:09:04 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header() {
+  return <h1>hello worlds</h1>;
+}
+
+function Logo(props)  {
+  const userPic = <img src={logo} />;
+  return userPic;
+}
+
+function App(props) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+  <Header/>
+  <h1>{props.title}</h1>
+   <Logo/>
+  </div>
   );
 }
 
